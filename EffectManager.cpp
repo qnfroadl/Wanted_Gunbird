c0481c5a6dc2a64c7a5163ce08014129657d7011
qnfroadl <qnfroadl@gmail.com>
2025-04-02 17:15:45 +0900

    Item매니저 업뎃,렌더

---
+++ EffectManager.cpp
@@ -0,0 +1,41 @@
+#include "EffectManager.h"
+
+
+void Effect::SetEffectKey(EEffectType key)
+{
+}
+
+void Effect::Update()
+{
+}
+
+void Effect::Render()
+{
+
+}
+
+
+void EffectManager::Init()
+{
+}
+
+void EffectManager::Update()
+{
+	
+}
+
+void EffectManager::Render()
+{
+
+}
+
+void EffectManager::Release()
+{
+
+}
+
+void EffectManager::PlayEffect(const FPOINT& pos, EEffectType key)
+{
+
+}
+
